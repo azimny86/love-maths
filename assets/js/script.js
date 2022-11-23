4bf9f0cfa33519570d6a18fc199d91d26e1d3b45
azimny86 <azimny86@gmail.com>
2022-11-23 18:21:31 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,19 +6,27 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!")
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`)
+                runGame(gameType);
             }
-        })
+        });
     }
-})
+    runGame("addition")
+});
 
 /**
- * Tha main game "loop", called when script is first loaded 
+ * The main game "loop", called when script is first loaded 
  * and after the user's answer has been processed 
  */
-function runGame() {
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25)+1;
     let num2 = Math.floor(Math.random() * 25)+1
+
+    if(gameType==="addition") {
+        displayAdditionQuestion(num1,num2);
+    } else {
+        alert(`Unknow game type; ${gameType}`);
+        throw `Unknow game type: ${gameType}. Aborting!`
+    }
 }
 
 function checkAnswer() {
@@ -37,8 +45,10 @@ function incremenWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 }
 
 function displaySubtractQuestion() {
